db99ad96451f6e7037e8cb2e55e7ad0219a43f29
Nirav Patel <niravpatelp129@gmail.com>
2019-01-24 15:55:36 -0500

    asd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var socket = require('socket.io');
 var app = express();
-
+var connections = []
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/page.html');
 });
@@ -10,8 +10,10 @@ app.use(express.static('public'));
 
 var server = app.listen(process.env.PORT || 3000, console.log("Site is up on port 3000"))
 var io = socket(server)
+var clients = io.sockets.clients();
 
 io.on('connection', function(socket){
+  connections.push(socket.id);
   console.log('somebody connected: ' + socket.id);
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
@@ -21,7 +23,14 @@ io.on('connection', function(socket){
   })
   socket.on('draw', function(input){
     io.emit('draw', input);
-    // socket.to('draw').emit('draw', input);
-    console.log(input)
   })
+  socket.on('disconnect', function(){
+    connections.splice(connections.indexOf(socket.id), 1);
+  })
+
+  socket.on('whodraws', function(){
+    io.emit('whodraws', connections);
+  });
+
+
 });
